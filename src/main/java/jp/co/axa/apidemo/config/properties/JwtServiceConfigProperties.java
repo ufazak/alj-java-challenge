2dbe8cc390e7764c9bee7daf8eb241c76bb71f6f
Toktar <ufazak@gmail.com>
2023-06-05 00:31:13 +0900

    [api-demo] removed unused imports

--- src/main/java/jp/co/axa/apidemo/config/properties/JwtServiceConfigProperties.java
+++ src/main/java/jp/co/axa/apidemo/config/properties/JwtServiceConfigProperties.java
@@ -1,7 +1,5 @@
 package jp.co.axa.apidemo.config.properties;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
